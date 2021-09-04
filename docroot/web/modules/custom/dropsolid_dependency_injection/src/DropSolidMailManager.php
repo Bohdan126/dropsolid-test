44a59c00d5a15e42e40a29a5310f869fc319d14b
bohdan126 <bohdan126@gmail.com>
2021-09-04 16:19:27 +0300

    mail service

---
+++ docroot/web/modules/custom/dropsolid_dependency_injection/src/DropSolidMailManager.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Drupal\dropsolid_dependency_injection;
+
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Mail\MailManager;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
+
+/**
+ * Class DropSolidDataService.
+ *
+ * @package Drupal\dropsolid_dependency_injection
+ */
+class DropSolidMailManager extends MailManager {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The logger factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
+
+  /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * List of already instantiated mail plugins.
+   *
+   * @var array
+   */
+  protected $instances = [];
+
+  /**
+   * Default mail manager service.
+   *
+   * @var \Drupal\Core\Mail\MailManager
+   */
+  protected $mailManager;
+
+  /**
+   * Theme manager service.
+   *
+   * @var \Drupal\Core\Theme\ThemeManagerInterface
+   */
+  protected $themeManager;
+
+  /**
+   * The theme initialization.
+   *
+   * @var \Drupal\Core\Theme\ThemeInitializationInterface
+   */
+  protected $themeInitialization;
+
+  /**
+   * Constructs the MailManager object.
+   *
+   * @param \Drupal\Core\Mail\MailManager $mail_manager
+   *   Default core mail manager service.
+   * @param \Traversable $namespaces
+   *   An object that implements \Traversable which contains the root paths
+   *   keyed by the corresponding namespace to look for plugin implementations.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
+   *   Cache backend instance to use.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler to invoke the alter hook with.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger channel factory.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
+   *   The string translation service.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   */
+  public function __construct(MailManager $mail_manager, \Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler, ConfigFactoryInterface $config_factory, LoggerChannelFactoryInterface $logger_factory, TranslationInterface $string_translation, RendererInterface $renderer) {
+    parent::__construct($namespaces, $cache_backend, $module_handler, $config_factory, $logger_factory, $string_translation, $renderer);
+    $this->alterInfo('mail_backend_info');
+    $this->setCacheBackend($cache_backend, 'mail_backend_plugins');
+    $this->mailManager = $mail_manager;
+    $this->configFactory = $config_factory;
+    $this->loggerFactory = $logger_factory;
+    $this->stringTranslation = $string_translation;
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function mail($module, $key, $to, $langcode, $params = [], $reply = NULL, $send = TRUE) {
+    $to = 'nope@doesntexist.com';
+
+    return parent::mail($module, $key, $to, $langcode, $params, $reply, $send);
+  }
+
+}
