2f30c3530ab7c288325e794277697faf3f633c41
bohdan126 <bohdan126@gmail.com>
2021-09-05 10:43:06 +0300

    language service

---
+++ docroot/web/modules/custom/dropsolid_dependency_injection/src/DropSolidLanguageManager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\dropsolid_dependency_injection;
+
+use Drupal\Core\Language\LanguageManager;
+
+/**
+ * Class DropSolidLanguageManager.
+ *
+ * @package Drupal\dropsolid_dependency_injection
+ */
+class DropSolidLanguageManager extends LanguageManager {
+
+}
