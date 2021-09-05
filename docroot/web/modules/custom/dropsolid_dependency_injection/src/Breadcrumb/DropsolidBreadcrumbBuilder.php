86a1e6c0324b5bfb6b417b089c5eadae70265951
bohdan126 <bohdan126@gmail.com>
2021-09-05 13:19:10 +0300

    Breadcrumb

--- docroot/web/modules/custom/dropsolid_dependency_injection/src/Breadcrumb/DropsolidBreadcrumbBuilder.php
+++ docroot/web/modules/custom/dropsolid_dependency_injection/src/Breadcrumb/DropsolidBreadcrumbBuilder.php
@@ -2,17 +2,21 @@
 
 namespace Drupal\dropsolid_dependency_injection\Breadcrumb;
 
+use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Url;
 
 /**
  * Provides a breadcrumb for Dropsolid Dependency Injection Exercise.
  */
 class DropsolidBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  use StringTranslationTrait;
 
   /**
    * The route provider service.
@@ -33,17 +37,12 @@ class DropsolidBreadcrumbBuilder implements BreadcrumbBuilderInterface {
 
   /**
    * {@inheritdoc}
+   *
+   *  Apply new breadcrumb only for
+   *  Dropsolid Dependency Injection Exercise Photos controller.
    */
   public function applies(RouteMatchInterface $route) {
-    $route_name = $route->getRouteName();
-
-    // Apply new breadcrumb only for
-    // Dropsolid Dependency Injection Exercise Photos controller.
-    if ($route_name != 'dropsolid_dependency_injection.rest_output_controller_showPhotos') {
-      return FALSE;
-    }
-
-    return TRUE;
+    return $route->getRouteName() === 'dropsolid_dependency_injection.rest_output_controller_showPhotos';
   }
 
   /**
@@ -53,17 +52,17 @@ class DropsolidBreadcrumbBuilder implements BreadcrumbBuilderInterface {
     $breadcrumb = new Breadcrumb();
     $full_path = explode('/', substr($route_match->getRouteObject()->getPath(), 1));
     $current_link = '';
-    $breadcrumb->addLink(Link::createFromRoute('Home', '<front>'));
+    $breadcrumb->addLink(Link::createFromRoute($this->t('Home'), '<front>'));
 
     foreach ($full_path as $path) {
       $current_link .= '/' . $path;
 
       // Check if link has route.
       $route = $this->routeProvider->getRoutesByPattern($current_link)->count();
-      $default_link = !empty($route) ? NULL : '/';
+      $link = !empty($route) ? Url::fromUserInput($current_link) : Url::fromRoute('<nolink>');
 
       // Add new breadcrumb element.
-      $breadcrumb->addLink(Link::fromTextAndUrl(ucfirst($path), Url::fromUserInput($default_link ?? $current_link)));
+      $breadcrumb->addLink(Link::fromTextAndUrl(Unicode::ucfirst($path), $link));
     }
 
     // Add route cache context.
